5394c025d0a81ca314c929fe9c9a25418ff751f5
pramudjijoenatan <133725485+pramudjijoenatan@users.noreply.github.com>
2024-01-30 14:55:57 +0800

    Clean code

--- app/src/main/java/com/example/expensetrackerjavagroovy/controller/DBController.java
+++ app/src/main/java/com/example/expensetrackerjavagroovy/controller/DBController.java
@@ -1,33 +1,7 @@
 package com.example.expensetrackerjavagroovy.controller;
 
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.IntentSender;
-import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.database.DatabaseErrorHandler;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.UserHandle;
 import android.util.Log;
-import android.view.Display;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.example.expensetrackerjavagroovy.AllDataCallback;
 import com.example.expensetrackerjavagroovy.LoginCallBack;
@@ -36,12 +10,6 @@ import com.example.expensetrackerjavagroovy.model.Record;
 
 import org.bson.Document;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
